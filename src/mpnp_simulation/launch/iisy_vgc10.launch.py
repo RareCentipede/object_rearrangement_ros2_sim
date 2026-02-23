ca8e28db605572c70b558fc26aba4138a6bb2527
Zhengyang Lu <stevenlu176@gmail.com>
2026-02-23 22:11:35 +0000

    Create iisy_vgc10 moveit config package and the arm works.

---
+++ src/mpnp_simulation/launch/iisy_vgc10.launch.py
@@ -0,0 +1,130 @@
+import os
+
+from ament_index_python import get_package_prefix
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, AppendEnvironmentVariable, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description() -> LaunchDescription:
+    ld = LaunchDescription()
+
+    omnirob_iisy_moveit_pkg_share = FindPackageShare("iisy_vgc10_moveit_config")
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+            [FindExecutable(name="xacro"), 
+             ' ',
+             PathJoinSubstitution(
+                 [omnirob_iisy_moveit_pkg_share, 
+                  "config", "iisy_vgc10.urdf.xacro"]
+                 )
+             ]
+        )
+    robot_description = {'robot_description': robot_description_content}
+
+    # Run all nodes
+    robot_controllers = PathJoinSubstitution(
+        [
+            omnirob_iisy_moveit_pkg_share,
+            "config",
+            "ros2_controllers.yaml"
+        ]
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description]
+    )
+
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            '-topic', 'robot_description',
+            '-name', 'kuka-omnirob-iisy',
+            '-allow_renaming', 'true'
+        ]
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster",
+                   "--controller-manager", "/controller_manager",
+                   "--param-file", robot_controllers],
+        output="screen",
+    )
+
+    arm_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["arm_controller",
+                   "--controller-manager", "/controller_manager",
+                   "--param-file", robot_controllers],
+        output="screen",
+    )
+
+    # omnirob_controller_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["omnirob_controller_with_rotation",
+    #                "--controller-manager", "/controller_manager",
+    #                "--param-file", robot_controllers],
+    #     output="screen",
+    # )
+
+    # Bridge
+    bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        output="screen",
+        arguments=[
+            "/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model",
+            "/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V",
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'
+        ],
+    )
+
+    ros_gz_sim_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare("ros_gz_sim"), "launch", "gz_sim.launch.py"]
+            )        ),
+        launch_arguments=[('gz_args', [' -r -v 1 empty.sdf'])])
+
+    spawner_event_handler = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=gz_spawn_entity,
+            on_exit=[joint_state_broadcaster_spawner],
+        )
+    )
+    joint_state_broadcaster_spawner_event_handler = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[arm_controller_spawner],
+        )
+    )
+    # arm_controller_spawner_event_handler = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=arm_controller_spawner,
+    #         on_exit=[omnirob_controller_spawner],
+    #     )
+    # )
+
+    ld.add_action(ros_gz_sim_launch)
+    ld.add_action(spawner_event_handler)
+    # ld.add_action(arm_controller_spawner_event_handler)
+    ld.add_action(joint_state_broadcaster_spawner_event_handler)
+    ld.add_action(bridge)
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(gz_spawn_entity)
+
+    return ld
---
+++ src/mpnp_simulation/launch/iisy_vgc10_moveit.launch.py
@@ -0,0 +1,94 @@
+import os
+
+from moveit_configs_utils import MoveItConfigsBuilder
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch_ros.actions import Node
+
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+# Define xacro mappings for the robot description file
+def generate_launch_description():
+    launch_arguments = {
+        "robot_ip": "xxx.yyy.zzz.www",
+        "use_fake_hardware": "false",
+        "dof": "6",
+        "use_gazebo": "true"
+    }
+
+    # Load the robot configuration
+    moveit_config = (
+        MoveItConfigsBuilder(
+            robot_name="iisy_vgc10",
+            package_name="iisy_vgc10_moveit_config"
+        )
+        .robot_description(mappings=launch_arguments) #type: ignore
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .planning_scene_monitor(
+            publish_robot_description=True, publish_robot_description_semantic=True
+        )
+        .planning_pipelines(
+            pipelines=["ompl", "stomp", "pilz_industrial_motion_planner"]
+        )
+        .to_moveit_configs()
+    )
+
+    run_move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[moveit_config.to_dict(),
+                        {"publish_planning_scene_hz": 30.0},
+                        {"allow_trajectory_execution": True},
+                        {"use_sim_time": True},
+                        {"publish_planning_scene": True},
+                        {"publish_state_updates": True},
+                        {"publish_transforms_updates": True}
+                    ]
+    )
+
+    rviz_config_arg = DeclareLaunchArgument(
+        name="rviz_config",
+        default_value=PathJoinSubstitution(
+            [FindPackageShare("iisy_vgc10_moveit_config"), "config", "moveit.rviz"]
+        ),
+        description="Path to the RViz config file"
+    )
+    rviz_base = LaunchConfiguration("rviz_config")
+    rviz_config = PathJoinSubstitution(
+        [FindPackageShare("iisy_vgc10_moveit_config"), "config", rviz_base]
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.planning_pipelines,
+            moveit_config.joint_limits
+        ]
+    )
+
+    iisy_vgc10_gz_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare("mpnp_simulation"), "launch", "iisy_vgc10.launch.py"]
+            )
+        )
+    )
+
+    return LaunchDescription(
+        [
+            rviz_config_arg,
+            rviz_node,
+            run_move_group_node,
+            iisy_vgc10_gz_launch,
+        ]
+    )
