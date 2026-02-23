0b09eda8b0f82e9d6483892ec41f434de93d61c6
Zhengyang Lu <stevenlu176@gmail.com>
2026-02-23 23:14:41 +0000

    Create full moveit assembly.

---
+++ src/omnirob_iisy_vgc10_moveit_config/launch/demo.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_demo_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("kuka_omnirob_iisy_vgc10", package_name="omnirob_iisy_vgc10_moveit_config").to_moveit_configs()
+    return generate_demo_launch(moveit_config)
