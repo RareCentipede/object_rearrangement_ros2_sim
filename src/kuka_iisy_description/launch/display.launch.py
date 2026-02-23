b9e3be6b1804b44494dad748e48eea5aff1e87a7
Zhengyang Lu <stevenlu176@gmail.com>
2026-02-23 20:44:11 +0000

    Build standalone kuka_iisy_description package.

---
+++ src/kuka_iisy_description/launch/display.launch.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+from launch.event_handlers import OnProcessExit
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    pkg_share = FindPackageShare(package='kuka_iisy_description')
+    default_model_path = PathJoinSubstitution([pkg_share, 'urdf', 'lbr_iisy15_r930.urdf.xacro'])
+    default_rviz_config_path = PathJoinSubstitution([pkg_share, 'rviz', 'config.rviz'])
+
+    robot_description_content = Command(
+            [FindExecutable(name="xacro"), 
+             ' ',
+             default_model_path
+             ]
+        )
+    robot_description = {'robot_description': robot_description_content}
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[robot_description]
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[robot_description],
+        condition=UnlessCondition(LaunchConfiguration('gui'))
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=IfCondition(LaunchConfiguration('gui'))
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(name='gui', default_value='True', description='Flag to enable joint_state_publisher_gui'),
+        DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot model file'),
+        DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+        robot_state_publisher_node,
+        rviz_node
+    ])
